8e58a12ef7d1e1a9d058c1289634ee77042c8a04
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-06-21 14:11:59 -0500

    feat: controlador para el producto

--- src/main/java/com/isai/appdemospringsecuritycurso/controllers/ProductoController.java
+++ src/main/java/com/isai/appdemospringsecuritycurso/controllers/ProductoController.java
@@ -2,13 +2,11 @@ package com.isai.appdemospringsecuritycurso.controllers;
 
 import com.isai.appdemospringsecuritycurso.entitys.Producto;
 import com.isai.appdemospringsecuritycurso.repositository.ProductoRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,7 +28,8 @@ public class ProductoController {
 
 
     @PostMapping
-    public ResponseEntity<Producto> crearUnProducto(Producto producto) {
+    public ResponseEntity<Producto> crearUnProducto(
+            @RequestBody @Valid Producto producto) {
         return ResponseEntity.status(
                 HttpStatus.CREATED).body(
                 productoRepository.save(producto));
