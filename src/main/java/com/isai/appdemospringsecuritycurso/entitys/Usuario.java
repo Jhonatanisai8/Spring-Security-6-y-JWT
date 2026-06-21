7fd4bc73a23c4d094cc12f1f41dffb45c6f8b456
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-06-21 14:05:02 -0500

    feat: entidad Usuario

---
+++ src/main/java/com/isai/appdemospringsecuritycurso/entitys/Usuario.java
@@ -0,0 +1,26 @@
+package com.isai.appdemospringsecuritycurso.entitys;
+
+import com.isai.appdemospringsecuritycurso.util.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
