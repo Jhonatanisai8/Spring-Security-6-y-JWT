ebef7923be7e4e08494f3acec2118240c2f2eea3
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-06-21 13:50:17 -0500

    feat:  init

---
+++ src/main/java/com/isai/appdemospringsecuritycurso/AppDemoSpringSecurityCursoApplication.java
@@ -0,0 +1,13 @@
+package com.isai.appdemospringsecuritycurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDemoSpringSecurityCursoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppDemoSpringSecurityCursoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/isai/appdemospringsecuritycurso/AppDemoSpringSecurityCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.appdemospringsecuritycurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDemoSpringSecurityCursoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
